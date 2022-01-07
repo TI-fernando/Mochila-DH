c6df50fa721d4cad5bddbccf83ca4e6a5840533e
Fernando Henrique Silva <fernandoagro.654@gmail.com>
2022-01-07 13:43:47 -0300

    Alterado 07/01/22

--- Mochila/express/Aula3/app.js
+++ Mochila/express/Aula3/app.js
@@ -5,6 +5,8 @@ let app  =  express(); // declarar para poder usar o app.get ou app.use
 
 app.get('/', (req, res) => res.send ("Ola mundo!"));
 app.get('/contatos', (req, res) =>  res.send("Pagina contatos"));
+app.get('/ajuda', (req, res) => res.send ("Lembrar que res.send e para imprimir."));
+app.get('/eu', (req, res) => res.send ({nome:'Fernando', idade: 27}));
 
 app.use('/produtos', rotasProdutos);
 //Criando um servidor
